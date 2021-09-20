e2b35f9eec77fec8d7c9e9a5ab8feaa9d12bcfba
zhengbigbig <780357902@qq.com>
2021-09-20 17:29:14 +0800

    嵌套依赖

--- deps_2.ts
+++ deps_2.ts
@@ -8,7 +8,7 @@ import { readFileSync } from 'fs'
 import { resolve, relative, dirname } from 'path';
 
 // 设置根目录
-const projectRoot = resolve(__dirname, 'project_1')
+const projectRoot = resolve(__dirname, 'project_2')
 // 类型声明
 type DepRelation = { [key: string]: { deps: string[], code: string } }
 // 初始化一个空的 depRelation，用于收集依赖
@@ -38,12 +38,12 @@ function collectCodeAndDeps(filepath: string) {
         const depProjectPath = getProjectPath(depAbsolutePath)
         // 把依赖写进 depRelation
         depRelation[key].deps.push(depProjectPath)
+        collectCodeAndDeps(depAbsolutePath)
       }
     }
   })
 }
 // 获取文件相对于根目录的相对路径
 function getProjectPath(path: string) {
-  console.log(relative(projectRoot, path));
-  return relative(projectRoot, path).replace(/\\/g, '/')
+    return relative(projectRoot, path).replace(/\\/g, '/')
 }
