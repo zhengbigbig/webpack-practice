28999bc66b1196732b7c21c0a9b2b91e55ccda54
zhengbigbig <780357902@qq.com>
2021-09-20 16:29:50 +0800

    js文件转es5

--- demo1/file_to_es5.ts
+++ demo1/file_to_es5.ts
@@ -2,9 +2,9 @@ import {parse} from '@babel/parser';
 import * as babel from '@babel/core';
 import * as fs from 'fs';
 
-const code = `let a = 'let';let b = 2;const c = 3`;
+const code = fs.readFileSync('./test.js').toString();
 const ast = parse(code, {sourceType: 'module'});
 const result = babel.transformFromAstSync(ast, code, {
     presets: ['@babel/preset-env']
 });
-console.log(result.code);
+fs.writeFileSync('./test.es5.js', result.code);
