9b75b86597dfb6640ff4aba18ea2f934c4680e43
zhengbigbig <780357902@qq.com>
2021-09-22 21:02:46 +0800

    优化webpack.config.js

--- demo5/webpack.config.js
+++ demo5/webpack.config.js
@@ -1,6 +1,19 @@
 const ESLintPlugin = require('eslint-webpack-plugin')
 const path = require('path')
 
+const cssLoaders = (...loaders) => [
+    'style-loader',
+    {
+        loader: 'css-loader',
+        options: {
+            modules: {
+                compileType: 'icss',
+            }
+        }
+    },
+    ...loaders
+]
+
 module.exports = {
     mode: 'production',
     plugins: [
@@ -33,72 +46,39 @@ module.exports = {
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                compileType: 'icss',
-                            }
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            additionalData: `
+                use: cssLoaders({
+                    loader: 'sass-loader',
+                    options: {
+                        additionalData: `
                                 @import "@/scss-vars.scss";
                             `,
-                            sassOptions: {
-                                includePaths: [__dirname]
-                            }
+                        sassOptions: {
+                            includePaths: [__dirname]
                         }
-                    },
-                ]
+                    }
+                })
             },
             {
                 test: /\.less$/i,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                compileType: 'icss',
-                            }
-                        }
-                    },
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            additionalData: `
+                use: cssLoaders({
+                    loader: 'less-loader',
+                    options: {
+                        additionalData: `
                                 @import "@/less-vars.less";
                             `,
-                        }
-                    },
-                ]
+                    }
+                })
             },
             {
                 test: /\.styl(us)?$/i,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                compileType: 'icss',
-                            }
+                use: cssLoaders({
+                    loader: 'stylus-loader',
+                    options: {
+                        stylusOptions: {
+                            import: [path.resolve(__dirname, 'src/stylus-vars.styl')]
                         }
-                    },
-                    {
-                        loader: 'stylus-loader',
-                        options: {
-                            stylusOptions: {
-                                import: [path.resolve(__dirname,'src/stylus-vars.styl')]
-                            }
-                        }
-                    },
-                ]
+                    }
+                })
             }
         ]
     }
