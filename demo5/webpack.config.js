3bd43c2b7b1a3932b99255cbc8b4d17cc1acf9d3
zhengbigbig <780357902@qq.com>
2021-09-22 21:53:54 +0800

    优化之单独打包runtime

--- demo5/webpack.config.js
+++ demo5/webpack.config.js
@@ -31,6 +31,9 @@ module.exports = {
     output: {
         filename: '[name].[contenthash].js',
     },
+    optimization: {
+        runtimeChunk: 'single',
+    },
     resolve: {
         alias: {
             '@': path.join(__dirname, 'src/')
